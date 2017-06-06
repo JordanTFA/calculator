7666896d989572278a1fe4935a9289b9a6bb63b9
Chad Taljaardt <ctaljaardt@yahoo.co.uk>
2017-06-06 22:18:56 +0100

    Init

--- UserManagement/UserManagement/wsgi.py
+++ UserManagement/UserManagement/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for calculatorApp project.
+WSGI config for UserManagement project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calculatorApp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UserManagement.settings")
 
 application = get_wsgi_application()
